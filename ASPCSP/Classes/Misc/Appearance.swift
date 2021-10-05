e4868c0d13d7473dd6ac3a0d52a5eb0678870395
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-10-05 15:04:34 +0300

    build 1

---
+++ ASPCSP/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 255, green: 252, blue: 247)
+    
+    static let mainColor = UIColor(integralRed: 240, green: 126, blue: 45)
+    static let mainColorAlpha = UIColor(integralRed: 240, green: 126, blue: 45, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 254, green: 200, blue: 81)
+    
+    static let progress1Color = UIColor(integralRed: 240, green: 126, blue: 45)
+    static let progress2Color = UIColor(integralRed: 254, green: 200, blue: 81)
+    static let progress3Color = UIColor(integralRed: 0, green: 99, blue: 148)
+}
