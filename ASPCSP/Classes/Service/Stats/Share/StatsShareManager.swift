289b4a7eafec16f9d4fd5b11f416b251b42396ae
akonst17 <akonst17@gmail.com>
2022-04-14 18:08:40 +0300

    upgrade; build 4

--- ASPCSP/Classes/Service/Stats/Share/StatsShareManager.swift
+++ ASPCSP/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.asp.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.asp.widget")
     }
     
     @discardableResult
